278894cb8f68cf98368c24b2ec45377b2bd5d472
Eric Merida <emerida623@gmail.com>
2026-02-06 15:26:35 -0500

    Add files via upload

---
+++ makeText.js
@@ -0,0 +1,42 @@
+/** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const axios = require("axios");
+const { Markovmachine } = require("./markov.js");
+
+function makeText(path){
+  fs.readFile(path, "utf8", function cb(err, data) {
+    if (err) {
+      console.error(`Cannot read file: ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      generateText(data);
+    }
+  });
+
+}
+
+async function makeURLText(url){
+    let resp;
+
+    try {
+        resp = await axios.get(url);
+        generateText(resp.data);
+    } catch (err) {
+        console.error(`Cannot read URL: ${url}: ${err}`);
+        process.exit(1);
+    }
+    generateText(resp.data);
+}
+
+let [method, path] = process.argv.slice(2);
+
+if (method === "file") {
+    makeURLText(path);
+}
+else if (method === "url") {
+    makeURLText(path);
+}
+else {
+    console.error(`Unknown method: ${method}`);
+    process.exit(1);
+}
